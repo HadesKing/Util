7dabab2f0f95a35ba784c2d5acb0ac192a883aa1
刘迪 <Q1061321236@hotmail.com>
2020-08-14 14:40:31 +0800

    #SHA256和SHA512加密算法测试通过

--- src/Util.xUnitTests/EncryptionAlgorithm/ShaAlgorithmTests.cs
+++ src/Util.xUnitTests/EncryptionAlgorithm/ShaAlgorithmTests.cs
@@ -8,15 +8,20 @@ namespace Util.EncryptionAlgorithm.Tests
 {
     public class ShaAlgorithmTests
     {
-        private string m_original = "我是Chinese!@&123456";
+        //private string m_original = "我是Chinese!@&123456";
+        private string m_original = "123456";
+        private string m_filePath = @"C:\Users\Administrator\Downloads\example.txt";
 
         [Fact()]
         public void Encrypt256Test()
         {
             String ciphertext = ShaAlgorithm.Encrypt256(m_original);
 
-            Assert.NotNull(ciphertext);
-            Assert.NotEmpty(ciphertext);
+            bool result = (ciphertext.ToLower() == "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92");
+
+            Assert.True(result);
+            //Assert.NotNull(ciphertext);
+            //Assert.NotEmpty(ciphertext);
             //Assert.True(false, "This test needs an implementation");
         }
 
@@ -25,9 +30,31 @@ namespace Util.EncryptionAlgorithm.Tests
         {
             String ciphertext = ShaAlgorithm.Encrypt512(m_original);
 
-            Assert.NotNull(ciphertext);
-            Assert.NotEmpty(ciphertext);
+            bool result = (ciphertext.ToLower() == "ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413");
+
+            Assert.True(result);
+            //Assert.NotNull(ciphertext);
+            //Assert.NotEmpty(ciphertext);
             //Assert.True(false, "This test needs an implementation");
         }
+
+        [Fact()]
+        public void EncryptFile256Test()
+        {
+            String ciphertext = ShaAlgorithm.EncryptFile256(m_filePath);
+            bool result = (ciphertext.ToLower() == "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92");
+
+            Assert.True(result);
+        }
+
+        [Fact()]
+        public void EncryptFile512Test()
+        {
+            String ciphertext = ShaAlgorithm.EncryptFile512(m_filePath);
+            bool result = (ciphertext.ToLower() == "ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413");
+
+            Assert.True(result);
+        }
+
     }
 }
--- src/Util/EncryptionAlgorithm/ShaAlgorithm.cs
+++ src/Util/EncryptionAlgorithm/ShaAlgorithm.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
@@ -8,35 +7,118 @@ namespace Util.EncryptionAlgorithm
 {
     public class ShaAlgorithm
     {
+        private static Encoding Encoding => Encoding.UTF8;
 
-
-        public static String Encrypt256(String source)
+        private static byte[] GetBytes(String data)
         {
-            byte[] bytes = Encoding.UTF8.GetBytes(source);
-            byte[] hash = SHA256Managed.Create().ComputeHash(bytes);
+            return Encoding.GetBytes(data);
+        }
 
+        /// <summary>
+        /// 将hash byte转换成String
+        /// </summary>
+        /// <param name="hashBytes"></param>
+        /// <returns></returns>
+        private static String ConvertToString(byte[] hashBytes)
+        {
             StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < hash.Length; i++)
+
+            foreach (var t in hashBytes)
             {
-                builder.Append(hash[i].ToString("X2"));
+                builder.Append(t.ToString("X2"));
             }
 
             return builder.ToString();
         }
 
-        public static String Encrypt512(String source)
+        #region [Encrypt256]
+        /// <summary>
+        /// SHA 256加密
+        /// </summary>
+        /// <param name="original">明文</param>
+        /// <returns></returns>
+        public static String Encrypt256(String original)
         {
-            byte[] bytes = Encoding.UTF8.GetBytes(source);
-            byte[] hash = SHA512Managed.Create().ComputeHash(bytes);
-
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < hash.Length; i++)
-            {
-                builder.Append(hash[i].ToString("X2"));
-            }
-
-            return builder.ToString();
+            byte[] bytes = GetBytes(original);
+            return Encrypt256(bytes);
         }
+
+        /// <summary>
+        /// SHA 512加密
+        /// </summary>
+        /// <param name="bytes">明文转换之后的byte</param>
+        /// <returns></returns>
+        public static String Encrypt256(byte[] bytes)
+        {
+            byte[] hashBytes = SHA256Managed.Create().ComputeHash(bytes);
+
+            return ConvertToString(hashBytes);
+        }
+
+        public static String EncryptFile256(String filePath)
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+            FileStream fileStream = fileInfo.Open(FileMode.Open);
+            // Be sure it's positioned to the beginning of the stream.
+            fileStream.Position = 0;
+            // Compute the hash of the fileStream.
+
+            return Encrypt256(fileStream);
+        }
+
+        public static String Encrypt256(Stream stream)
+        {
+            byte[] hashBytes = SHA256Managed.Create().ComputeHash(stream);
+
+            return ConvertToString(hashBytes);
+        }
+
+        #endregion
+
+        #region [Encrypt512]
+        /// <summary>
+        /// SHA 512加密
+        /// </summary>
+        /// <param name="original">明文</param>
+        /// <returns></returns>
+        public static String Encrypt512(String original)
+        {
+            byte[] bytes = GetBytes(original);
+            return Encrypt512(bytes);
+        }
+
+        /// <summary>
+        /// SHA 512加密
+        /// </summary>
+        /// <param name="bytes">明文转换之后的byte</param>
+        /// <returns></returns>
+        public static String Encrypt512(byte[] bytes)
+        {
+            byte[] hashBytes = SHA512Managed.Create().ComputeHash(bytes);
+
+            return ConvertToString(hashBytes);
+        }
+
+        public static String EncryptFile512(String filePath)
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+            FileStream fileStream = fileInfo.Open(FileMode.Open);
+            // Be sure it's positioned to the beginning of the stream.
+            fileStream.Position = 0;
+            // Compute the hash of the fileStream.
+
+            return Encrypt512(fileStream);
+        }
+
+        public static String Encrypt512(Stream stream)
+        {
+            byte[] hashBytes = SHA512Managed.Create().ComputeHash(stream);
+
+            return ConvertToString(hashBytes);
+        }
+
+
+        #endregion
 
     }
 }
