26ce98a56f726d562fb0790c51497ef8c19a6f20
刘迪 <Q1061321236@hotmail.com>
2020-08-14 10:52:16 +0800

    添加SHA加密算法

---
+++ src/Util.xUnitTests/EncryptionAlgorithm/ShaAlgorithmTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using Util.EncryptionAlgorithm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Util.EncryptionAlgorithm.Tests
+{
+    public class ShaAlgorithmTests
+    {
+        private string m_original = "我是Chinese!@&123456";
+
+        [Fact()]
+        public void Encrypt256Test()
+        {
+            String ciphertext = ShaAlgorithm.Encrypt256(m_original);
+
+            Assert.NotNull(ciphertext);
+            Assert.NotEmpty(ciphertext);
+            //Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void Encrypt512Test()
+        {
+            String ciphertext = ShaAlgorithm.Encrypt512(m_original);
+
+            Assert.NotNull(ciphertext);
+            Assert.NotEmpty(ciphertext);
+            //Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
---
+++ src/Util/EncryptionAlgorithm/ShaAlgorithm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Util.EncryptionAlgorithm
+{
+    public class ShaAlgorithm
+    {
+
+
+        public static String Encrypt256(String source)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(source);
+            byte[] hash = SHA256Managed.Create().ComputeHash(bytes);
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                builder.Append(hash[i].ToString("X2"));
+            }
+
+            return builder.ToString();
+        }
+
+        public static String Encrypt512(String source)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(source);
+            byte[] hash = SHA512Managed.Create().ComputeHash(bytes);
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                builder.Append(hash[i].ToString("X2"));
+            }
+
+            return builder.ToString();
+        }
+
+    }
+}
